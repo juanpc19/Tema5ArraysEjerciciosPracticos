6a0e491f7fed175a42ac03a8fc9ec576ccb96e9c
juanpesca19 <juanpesca19@DESKTOP-278S0F8>
2023-02-09 22:07:48 +0100

    funcion creaArray acabada comentar y siguiente funcion

--- Tema5ArraysEjerciciosPracticos/src/ejercicio1/FuncionesEj1.java
+++ Tema5ArraysEjerciciosPracticos/src/ejercicio1/FuncionesEj1.java
@@ -4,7 +4,11 @@ import java.util.Arrays;
 
 public class FuncionesEj1 {
 
-	static char[] creaArray(int longitud, int minas) {
+	static int longitud = 0;
+
+	static int minas = 0;
+
+	static char[] creaArray() {
 
 		char tablaCreada[] = new char[longitud];
 
@@ -12,33 +16,39 @@ public class FuncionesEj1 {
 
 		int contador = 0;
 
+		for (int posicion = 0; posicion < tablaCreada.length; posicion++) {
+			tablaCreada[posicion] = '0';
+		}
+
 		while (contador < minas) {
+
 			posInsercionAleatoria = (int) (Math.random() * longitud);
+
 			if (tablaCreada[posInsercionAleatoria] != '*') {
 				tablaCreada[posInsercionAleatoria] = '*';
 				contador++;
 
+				if (posInsercionAleatoria != tablaCreada.length - 1 && tablaCreada[posInsercionAleatoria + 1] != '*') {
+					tablaCreada[posInsercionAleatoria + 1] = '1';
+				}
+
+				if (posInsercionAleatoria != 0 && tablaCreada[posInsercionAleatoria - 1] != '*') {
+					tablaCreada[posInsercionAleatoria - 1] = '1';
+				}
+			}
+
+		}
+
+		for (int posicion = 0; posicion < tablaCreada.length; posicion++) {
+
+			if (posicion != 0 && posicion != tablaCreada.length - 1 && tablaCreada[posicion - 1] == '*'
+					&& tablaCreada[posicion + 1] == '*' && tablaCreada[posicion] != '*') {
+
+				tablaCreada[posicion] = '2';
 			}
 		}
 
-		//dividir for en 3, for 0 , for 1 y for 2 , cada uno empezando por una posicion diferente
-		for (int posicion = 0; posicion < tablaCreada.length; posicion++) {
-
-			if ((tablaCreada[posicion] == '*') && (tablaCreada[posicion] != tablaCreada[tablaCreada.length - 1])) {
-				tablaCreada[posicion + 1] = '1';
-				
-			} else if ((tablaCreada[posicion] != tablaCreada[0])
-					&& (tablaCreada[posicion] != '1')
-					&& (tablaCreada[posicion - 1] == '*')
-					&& (tablaCreada[posicion + 1] == '*')) {
-				tablaCreada[posicion] = '2';
-			} 
-			
-
-			
-			
-		}
-
+		//print para comprobar funcion quitar al finalizar ejercicio
 		System.out.println(Arrays.toString(tablaCreada));
 
 		return tablaCreada;
--- Tema5ArraysEjerciciosPracticos/src/ejercicio1/Main.java
+++ Tema5ArraysEjerciciosPracticos/src/ejercicio1/Main.java
@@ -1,19 +1,26 @@
 package ejercicio1;
 
-import java.util.Arrays;
+
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 
-		int longitudUsuario=20;
+		Scanner dogma = new Scanner(System.in);
 
-		int minasUsuario=6;
+		System.out.println("¿Cuantas casillas desea en su tablero? ");
 
-		FuncionesEj1.creaArray(longitudUsuario, minasUsuario);
-		
-		
-	
+		FuncionesEj1.longitud = dogma.nextInt();
+
+		System.out.println("¿Cuantas minas desea en su tablero? ");
+
+		FuncionesEj1.minas = dogma.nextInt();
+
+		FuncionesEj1.creaArray();
+
+		dogma.close();
+
 	}
 
 }
