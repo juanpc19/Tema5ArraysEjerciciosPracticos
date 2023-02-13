e1414be855d9226ed8e85078ba46acf11e0462da
juanpc19 <juan.perez@iesnervion.es>
2023-02-13 14:37:53 +0100

    retoques y comentario de main

--- Tema5ArraysEjerciciosPracticos/src/ejercicio2/FuncionesEj2.java
+++ Tema5ArraysEjerciciosPracticos/src/ejercicio2/FuncionesEj2.java
@@ -22,35 +22,48 @@ public class FuncionesEj2 {
 
 		double puntuacion = 0;
 
-		int decisionJugador = 0;
+		String decisionJugador = "";
 
 		Scanner dogma = new Scanner(System.in);
 
 		do {
 
-			System.out.println("¿Desea sacar una carta? De ser asi pulse 1, en caso contrario pulse 0 para plantarse.");
+			System.out.println();
+			System.out.println("¿Desea sacar una carta? Indique con 'si' o 'no' ");
 
-			decisionJugador = dogma.nextInt();
+			decisionJugador = dogma.next();
 
-			if (decisionJugador == 1) {
-				carta = (int) (Math.random() * 11);
+			if (decisionJugador.equals("si")) {
 
+				carta = (int) (Math.random() * 10);
 				puntuacion += baraja[carta];
+
+				System.out.println();
+				System.out.println("El valor de la carta sacada es: " + baraja[carta]);
+				System.out.println();
+				System.out.println("Su puntuacion actual es: " + puntuacion);
 			}
 
-		} while (puntuacion <= 7.5 && decisionJugador != 0);
+		} while (puntuacion <= 7.5 && decisionJugador.equals("si"));
 
 		return puntuacion;
+	}
+
+	static void menor8(double valorPuntuacion) {
+
+		if (valorPuntuacion >= 8.0) {
+			System.out.println("¡Te has pasado! ");
+		}
 	}
 
 	static int decideGanador() {
 
 		int ganador = 0;
 
-		if (puntuacionJugador1 < puntuacionJugador2) {
+		if (puntuacionJugador1 < puntuacionJugador2 && puntuacionJugador2 <= 7.5) {
 			ganador = 2;
 
-		} else if (puntuacionJugador1 > puntuacionJugador2) {
+		} else if (puntuacionJugador1 > puntuacionJugador2 || puntuacionJugador2 >= 8.0) {
 			ganador = 1;
 
 		} else {
@@ -60,6 +73,8 @@ public class FuncionesEj2 {
 		return ganador;
 	}
 
+	// sacar tanto como sea posible de aqui yponer en main llamando a funciones con
+	// clase nombre funcion
 	static void ejecutaJuego() {
 
 		System.out.println("¡Comienza el juego!");
@@ -70,25 +85,36 @@ public class FuncionesEj2 {
 
 		puntuacionJugador1 = sacaCarta();
 
+		menor8(puntuacionJugador1);
+
 		System.out.println();
 
-		System.out.println("Es el turno del jugador 2. ");
-
-		puntuacionJugador2 = sacaCarta();
-
-		switch (decideGanador()) {
-
-		case 2 -> {
+		if (puntuacionJugador1 >= 8) {
 			System.out.println("El ganador es el jugador 2.");
-		}
-		case 1 -> {
-			System.out.println("El ganador es el jugador 1.");
-		}
-		case 0 -> {
-			System.out.println("El resultado es empate.");
-		}
-		}
 
+		} else {
+
+			System.out.println("Es el turno del jugador 2. ");
+
+			puntuacionJugador2 = sacaCarta();
+
+			System.out.println();
+
+			menor8(puntuacionJugador2);
+
+			switch (decideGanador()) {
+
+			case 2 -> {
+				System.out.println("El ganador es el jugador 2.");
+			}
+			case 1 -> {
+				System.out.println("El ganador es el jugador 1.");
+			}
+			case 0 -> {
+				System.out.println("El resultado es empate.");
+			}
+			}
+
+		}
 	}
-
 }
